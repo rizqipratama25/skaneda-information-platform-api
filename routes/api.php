d37f1bed3c5747562fdc14ed8d76132449cc615a
rizqipratama25 <rizqip501@gmail.com>
2025-09-15 18:46:25 +0700

    commit agenda, user

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AgendaController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,5 +15,15 @@ Route::prefix('auth')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
     Route::post('/register', [AuthController::class, 'register']);
-    Route::get('/email/verify')->middleware('auth:sanctum')->name('verification.notice');
 });
+
+// User
+Route::patch('/user/{id}', [RoleController::class, 'ubahRole'])->middleware(['auth:sanctum', 'can:admin']);
+Route::get('/users', [UserController::class, 'index'])->middleware(['auth:sanctum', 'can:admin']);
+
+// Role
+Route::post('/role', [RoleController::class, 'tambahRole'])->middleware(['auth:sanctum', 'can:admin']);
+Route::get('/roles', [RoleController::class, 'index'])->middleware(['auth:sanctum', 'can:admin']);
+
+// Agenda
+Route::post('/agenda', [AgendaController::class, 'buatAgenda']);
