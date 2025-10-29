5611abbaebac8d300d68f1b1b737297b85840ba2
rizqipratama25 <rizqip501@gmail.com>
2025-10-29 12:19:02 +0700

    sanctum fix

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,https://env-8080121.jh-beon.cloud,https://argillaceous-gwenn-overindulgent.ngrok-free.dev',
         Sanctum::currentApplicationUrlWithPort(),
         // Sanctum::currentRequestHost(),
     ))),
