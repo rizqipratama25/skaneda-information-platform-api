9a0a806454b084213de26a34492a58ed8d4e8e45
rizqipratama25 <rizqip501@gmail.com>
2025-10-29 10:47:59 +0700

    minor fix2

--- config/cors.php
+++ config/cors.php
@@ -1,13 +1,11 @@
 <?php
 return [
-    'paths' => ['api/*', 'auth/*', 'sanctum/csrf-cookie', 'email/*'],
+    'paths' => ['api/', 'auth/', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'https://argillaceous-gwenn-overindulgent.ngrok-free.dev', // domain FE Next.js kamu
-    ],
-    'allowed_origins_patterns' => ['*'],
-    'allowed_headers' => ['*'],
-    'exposed_headers' => ['*'],
+    'allowed_origins' => ['https://hyperanabolic-camren-subzero.ngrok-free.dev', 'https://argillaceous-gwenn-overindulgent.ngrok-free.dev'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization'],
+    'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true, // true kalau pakai cookie/session
 ];
